776be177571342f0cd8a748e10e5bcc37a841ffb
sahil <sahil.kabra@gmail.com>
2014-07-31 15:57:53 +0530

    pub sub implementation

--- node/apps/floor-plan/server/app/app.js
+++ node/apps/floor-plan/server/app/app.js
@@ -3,8 +3,12 @@ var morgan = require('morgan');
 var router = require('routes/router');
 var config = require('app/config');
 var app = express();
+var appEvent = require('app/pubSub');
+
 app.use(morgan('short'));
 app.use(router);
 //app.use(error);
-app.listen(config.express.port);
-console.log('Service (%d) started on port %d', config.pid, config.express.port);
+appEvent.on('plan.dbConnected', function() {
+        app.listen(config.express.port);
+        console.log('Service (%d) started on port %d', config.pid, config.express.port);
+});
--- node/apps/floor-plan/server/app/floorPlan/building/buildingModel.js
+++ node/apps/floor-plan/server/app/floorPlan/building/buildingModel.js
@@ -1,8 +1,10 @@
 var mongoClient = require('mongodb').MongoClient;
 var config = require('app/config').mongo;
+var appEvent = require('app/pubSub');
 
 var db; 
 mongoClient.connect(config.url, function(err, database) {
         if (err) throw err;
         db = database;
+        appEvent.emit('plan.dbConnected');
 });
