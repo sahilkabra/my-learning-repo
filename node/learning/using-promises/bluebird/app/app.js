01d287ef005d9703022c56d010b72b2f1d016468
sahilkabra <sahil.kabra@gmail.com>
2014-08-10 11:35:39 +0530

    Tabs to spaces

--- node/learning/using-promises/bluebird/app/app.js
+++ node/learning/using-promises/bluebird/app/app.js
@@ -8,23 +8,23 @@
 var Promise = require('bluebird');
 
 var students = [
-	{name: 'abc', id: 1},
-	{name: 'xyz', id: 2},
-	{name: 'pqr', id: 3}
+    {name: 'abc', id: 1},
+    {name: 'xyz', id: 2},
+    {name: 'pqr', id: 3}
 ];
 
 var getStudents = function(studentId) {
-		return new Promise(function(resolve, reject) {
-				var student;
+        return new Promise(function(resolve, reject) {
+                var student;
 
-				for (var i = 0; i < students.length; i++) {
-						if (students[i].id === studentId) {
-					            student = students[i];
+                for (var i = 0; i < students.length; i++) {
+                        if (students[i].id === studentId) {
+                                student = students[i];
                                 break;
-						}
-				}
+                        }
+                }
                 student? resolve(student) : reject('Not found');
-		});
+        });
 };
 
 exports.getStudents = getStudents;
