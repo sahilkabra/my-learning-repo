8aac34287816b5036d06d90c1bff6ac0deb3a300
sahil <sahil.kabra@gmail.com>
2014-08-12 13:42:45 +0530

    understanding promises

--- node/learning/using-promises/bluebird/app/app.js
+++ node/learning/using-promises/bluebird/app/app.js
@@ -1,30 +1,37 @@
 //app/app.js
 
 /*
- * A simple app that tests how promises can be used to have 
+ * A simple app that tests how promises can be used to have
  * node modules return promises
  */
 
 var Promise = require('bluebird');
 
 var students = [
-    {name: 'abc', id: 1},
-    {name: 'xyz', id: 2},
-    {name: 'pqr', id: 3}
+	{name: 'abc', id: 1},
+	{name: 'xyz', id: 2},
+	{name: 'pqr', id: 3}
 ];
 
-var getStudents = function(studentId) {
-        return new Promise(function(resolve, reject) {
-                var student;
+/**
+ * Returns the student object matching the specified criteria asynchronously.
+ * @param studentId: The student id
+ * @param callback: The callback function to be invoked once the student is fetched
+ *		This is optional, if no callback is specified a Promise will be returned.
+ */
+var getStudents = function(studentId, callback) {
+	var findStudent = function(resolve, reject) {
+		var student;
 
-                for (var i = 0; i < students.length; i++) {
-                        if (students[i].id === studentId) {
-                                student = students[i];
-                                break;
-                        }
-                }
-                student? resolve(student) : reject('Not found');
-        });
+		for (var i = 0; i < students.length; i++) {
+				if (students[i].id === studentId) {
+						student = students[i];
+						break;
+				}
+		}
+		student? resolve(student) : reject('Not found');
+	};
+	return new Promise(findStudent).nodeify(callback);
 };
 
 exports.getStudents = getStudents;
--- node/learning/using-promises/bluebird/test/appSpec.js
+++ node/learning/using-promises/bluebird/test/appSpec.js
@@ -5,12 +5,49 @@ var expect = chai.expect;
 chai.use(chaiAsPromised);
 
 describe('The getStudents async function', function() {
-        var app = require('app/app');
+		var app = require('app/app');
 
-        it('should return a student via the promise API', function() {
-                expect(app.getStudents(2)).to.eventually.deep.equal({name: 'xyz', id:2});
-        });
-        it('should throw via the promise API', function() {
-                expect(app.getStudents(6)).to.be.rejectedWith('Not found');
-        });
+		it('should return a student via the promise API', function() {
+				expect(app.getStudents(2)).to.eventually.deep.equal({name: 'xyz', id:2});
+		});
+		it('should throw via the promise API', function() {
+				expect(app.getStudents(6)).to.be.rejectedWith('Not found');
+		});
+});
+
+describe('Using Promises', function() {
+		var app = require('app/app');
+		describe('with promise then API', function() {
+			it('should return a student and callback should be called with then', function(done) {
+				var expectStudent = function(student) {
+					expect(student).to.have.property('name').and.equal('xyz');
+					done();
+				};
+				app.getStudents(2).then(expectStudent);
+			});
+			it('the reject function should be called', function(done) {
+				var expectStudentRej = function(e) {
+					expect(e).to.have.string('Not found');
+					done();
+				};
+				app.getStudents(8).catch(expectStudentRej);
+			});
+		});
+		describe('with node callback API', function() {
+			it('should return a student', function(done) {
+				var expectStudent = function(err, student) {
+					expect(err).to.not.exist;
+					expect(student).to.have.property('name').and.equal('xyz');
+					done();
+				};
+				app.getStudents(2, expectStudent);
+			});
+			it('should throw an error', function(done) {
+				var expectStudent = function(err, student) {
+					expect(err).to.have.string('Not found');
+					done();
+				};
+				app.getStudents(8, expectStudent);
+			});
+		});
 });
