419a53ac985c9c5d10a49e9f80849aaa65e0694e
Sahil Kabra <sahil.kabra@gmail.com>
2017-01-15 19:46:17 +1100

    forms module for 2 way binding in spec

--- angular2/angular-fundamentals/src/modules/hero/hero.component.spec.ts
+++ angular2/angular-fundamentals/src/modules/hero/hero.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {DebugElement} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import HeroComponent from './hero.component';
 import {expect} from 'chai';
 
@@ -12,6 +13,7 @@ describe('Hero Component', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [FormsModule],
             declarations: [HeroComponent],
         });
         fixture = TestBed.createComponent(HeroComponent);
