1c461cf72e6f7d6d98f30a2ed39009ffe9147d8d
Sahil Kabra <sahil.kabra@gmail.com>
2017-05-28 20:13:09 +1000

    first compose - not complete

---
+++ docker/using-compose/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello {name}. Hostname: {hostname}. I modified a line".format(
+        name=os.getenv("NAME"),
+        hostname=socket.gethostname())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
