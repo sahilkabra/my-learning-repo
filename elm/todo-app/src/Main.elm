e4429721442dd5d4ecd2ddbf7a653951cbb8bc54
Sahil Kabra <sahil.kabra@gmail.com>
2017-08-12 12:01:56 +1000

    Refactor elm in terms of state, action, reducer

--- elm/todo-app/src/Main.elm
+++ elm/todo-app/src/Main.elm
@@ -1,13 +1,42 @@
 module Main exposing (..)
 
-import Html exposing (div)
+import Html exposing (Html, program, div)
 
 
-main : Html.Html Msg
+main : Program Never State Action
 main =
-    div []
-        [ Html.text "Hello World" ]
+    program
+        { init = init
+        , view = view
+        , update = reducer
+        , subscriptions = subscriptions
+        }
 
 
-type Msg
+type alias State =
+    String
+
+
+type Action
     = Noop
+
+
+init : ( State, Cmd Action )
+init =
+    ( "Hello", Cmd.none )
+
+
+view : State -> Html Action
+view model =
+    div []
+        [ Html.text model ]
+
+
+reducer : Action -> State -> ( State, Cmd Action )
+reducer msg state =
+    ( state, Cmd.none )
+
+
+subscriptions : State -> Sub Action
+subscriptions state =
+    Sub.none
